bf5afeaa30a1b155897cee28a236ea6dbc98db8b
HudsonTerroso <139827928+HudsonTerroso@users.noreply.github.com>
2024-05-01 21:31:06 -0300

    inicial commit

---
+++ src/main/java/br/com/portifolio/crud/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package br.com.portifolio.crud.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id){
+		super("Usuário não localizado com a id " + id);
+	}
+
+}
